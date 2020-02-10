94ad2200be5b510e3475bf14f8eda141d15c078c
zhaoshenglong <G245078728@sjtu.edu.cn>
2020-02-10 15:20:22 +0800

    Add P88 Merge Sorted Array T99.44S100

---
+++ python/EasyProblems/MergeSortedArray.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+        while j >= 0:
+            nums1[k] = nums2[j]
+            k -= 1
+            j -= 1
+
+
+def main():
+    s = Solution()
+    a = [1, 2, 3, 0, 0, 0]
+    s.merge(a, 3, [2, 5, 6], 3)
+    print(a)
+
+
+if __name__ == '__main__':
+    main()
