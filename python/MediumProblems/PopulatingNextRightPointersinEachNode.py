ebecb596315cf2f60612f7efeae369469b41d578
zhaoshenglong <G245078728@sjtu.edu.cn>
2020-02-15 21:45:04 +0800

    Add P116 Populating Next Right Pointers in Each Node, T86.39S100

---
+++ python/MediumProblems/PopulatingNextRightPointersinEachNode.py
@@ -0,0 +1,24 @@
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution:
+    def connect(self, root: Node) -> Node:
+        if not root:
+            return root
+        p = root
+        if root.left:
+            while root:
+                root.left.next = root.right
+                if root.next:
+                    root.right.next = root.next.left
+                root = root.next
+        self.connect(p.left)
+        return p
