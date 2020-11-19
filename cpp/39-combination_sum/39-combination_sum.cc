dff3109b2287ddc99227aea7c90460f0a6c06b27
zhaoshenglong <G245078728@sjtu.edu.cn>
2020-11-19 11:54:33 +0800

    Add another solution which only search forward and it solve the duplicate problem

--- cpp/39-combination_sum/39-combination_sum.cc
+++ cpp/39-combination_sum/39-combination_sum.cc
@@ -9,13 +9,17 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        return second_sol(candidates, target, 0); 
+    }
+
+    vector<vector<int>> first_solution(vector<int>& candidates, int target){
         set<vector<int>> res_set;
         int sz = candidates.size();
         int i;
         
         for (i = 0; i < sz; i++) {
             if (candidates[i] < target) {
-                for (auto v : combinationSum(candidates, target - candidates[i])) {
+                for (auto v : first_solution(candidates, target - candidates[i])) {
                     v.push_back(candidates[i]);
                     sort(v.begin(), v.end());
                     res_set.insert(v);
@@ -26,6 +30,24 @@ public:
         }
 
         return vector<vector<int>>(res_set.begin(), res_set.end());
+    }
+
+    vector<vector<int>> second_sol(vector<int>& candidates, int target, int start) {
+        vector<vector<int>> res;
+        int sz = candidates.size();
+        int i;
+
+        for (i = start; i < sz; i++) {
+            if (candidates[i] < target) {
+                for (auto v : second_sol(candidates, target - candidates[i], i)) {
+                    v.push_back(candidates[i]);
+                    res.push_back(v);
+                }
+            } else if (candidates[i] == target) {
+                res.push_back(vector<int>(1, target));
+            }
+        }
+        return res;
     }
 };
 
