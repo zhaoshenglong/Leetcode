e3dc60184fd6d28d489ba4bb8d877f8b0cee46ed
zhaoshenglong <G245078728@sjtu.edu.cn>
2022-03-20 18:53:36 +0800

    Add solution of 606

---
+++ 606_easy_constructing_string_from_binary_tree/cpp/main.cpp
@@ -0,0 +1,26 @@
+#include "../../include/leetcode.hpp"
+
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    string tree2str(TreeNode* root) {
+        if (!root->left) {
+            if (!root->right)
+                return to_string(root->val) + "()";
+            return to_string(root->val) + "()" + tree2str(root->right);
+        }
+        if (!root->right)
+            return to_string(root->val) + "(" + tree2str(root->left) + ")";
+        return to_string(root->val) + "(" + tree2str(root->left) + ")" + tree2str(root->right);
+    }
+};
