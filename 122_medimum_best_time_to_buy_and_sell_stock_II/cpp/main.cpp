57d7c02fdd2b1d63c9cfcf79798b829eda1bf2c9
zhaoshenglong <G245078728@sjtu.edu.cn>
2022-03-27 18:19:01 +0800

    Add solution of 122

---
+++ 122_medimum_best_time_to_buy_and_sell_stock_II/cpp/main.cpp
@@ -0,0 +1,19 @@
+#include "../../include/leetcode.hpp"
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        int i = 1, p;
+        
+        while(i < prices.size()) {
+            p = i;
+            while(i + 1 < prices.size() && prices[i + 1] >= prices[i])
+                i++;
+            profit += prices[i] - prices[p];
+            i++;
+        }
+
+        return profit;
+    }
+};
